1021cd04474773caf34f7bfd5753dc22b27df139
Murat Aykanat <aykanatm@gmail.com>
2018-02-14 21:56:23 +0300

    Implemented Spring Content for streaming.

--- src/main/java/org/murat/test/App.java
+++ src/main/java/org/murat/test/App.java
@@ -2,10 +2,15 @@ package org.murat.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.content.commons.repository.Store;
+import org.springframework.content.rest.StoreRestResource;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    @StoreRestResource(path="test-videos")
+    public interface VideoStore extends Store<String> {}
 }
